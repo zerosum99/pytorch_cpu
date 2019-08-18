8e87420a41d29c055b804cc8c2a95b3bf2051921
zerosum99 <myjlms99@gmail.com>
2019-08-19 00:22:52 +0900

    99990000999000

---
+++ pytorch_import.py
@@ -0,0 +1,17 @@
+
+import_str = """
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torch.utils.data import DataLoader, TensorDataset
+
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+
+"""
